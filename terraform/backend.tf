4f116476902cd864afcd7ee543bbfefdc75d90a4
anilshetty20 <anilshetty6615@gmail.com>
2026-03-05 16:07:00 +0530

    New bucket without remote backend

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "github-actions-terraform-website-anil2025"
-    key            = "s3-static-website/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "github-actions-terraform-website-anil2025"
+  bucket = "github-actions-terraform-website-anil2024"
   tags = {
     Name = "GitHub Actions Demo"
   }
