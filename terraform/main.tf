af2ebe83fc56b1a0108cb2e4437adbaf0767e255
anilshetty20 <anilshetty6615@gmail.com>
2026-03-05 15:21:40 +0530

    Added backend.tf and bucketpolicy in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,23 +3,39 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-    bucket = "github-actions-terraform-website-anil2021"
-    tags = {
+  bucket = "github-actions-terraform-website-anil2026"
+  tags = {
     Name = "GitHub Actions Demo"
   }
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
-    bucket = aws_s3_bucket.website_bucket.id
+  bucket = aws_s3_bucket.website_bucket.id
 
-    index_document {
-      suffix = "index.html"
-    }
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
-    bucket = aws_s3_bucket.website_bucket.id
-    block_public_acls = false
-    block_public_policy = false
-  
+  bucket = aws_s3_bucket.website_bucket.id
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
 }
