b4649781584c96170d6a4464afd6b0f650036292
anilshetty20 <anilshetty6615@gmail.com>
2026-03-05 12:57:21 +0530

    terraform s3 project

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+    bucket = "github-actions-terraform-website-anil20"
+    tags = {
+    Name = "GitHub Actions Demo"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+    bucket = aws_s3_bucket.website_bucket.id
+
+    index_document {
+      suffix = "index.html"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.website_bucket.id
+    block_public_acls = false
+    block_public_policy = false
+  
+}
